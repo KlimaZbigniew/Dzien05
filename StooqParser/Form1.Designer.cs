36cdac339f7f695ba4c2b00aeda18595814181a7
kurs <kurs@DESKTOP-V3HDKPQ>
2021-07-31 16:34:20 +0200

    DataGridView

---
+++ StooqParser/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace StooqParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbTicker = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.dtStop = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbTicker
+            // 
+            this.tbTicker.Location = new System.Drawing.Point(107, 18);
+            this.tbTicker.Name = "tbTicker";
+            this.tbTicker.Size = new System.Drawing.Size(100, 20);
+            this.tbTicker.TabIndex = 0;
+            this.tbTicker.Text = "pkn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ticker:";
+            // 
+            // dtStart
+            // 
+            this.dtStart.Location = new System.Drawing.Point(156, 63);
+            this.dtStart.Name = "dtStart";
+            this.dtStart.Size = new System.Drawing.Size(200, 20);
+            this.dtStart.TabIndex = 2;
+            // 
+            // dtStop
+            // 
+            this.dtStop.Location = new System.Drawing.Point(448, 62);
+            this.dtStop.Name = "dtStop";
+            this.dtStop.Size = new System.Drawing.Size(200, 20);
+            this.dtStop.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Data początkowa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(362, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Data końcowa:";
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(665, 18);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(89, 45);
+            this.btnGet.TabIndex = 6;
+            this.btnGet.Text = "Pobierz";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(64, 129);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvData.Size = new System.Drawing.Size(690, 253);
+            this.dgvData.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtStop);
+            this.Controls.Add(this.dtStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTicker);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbTicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.DateTimePicker dtStop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.DataGridView dgvData;
+    }
+}
+
---
+++ StooqParser/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StooqParser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtStop.Value = DateTime.Now;
+            dtStart.Value = DateTime.Now.AddMonths(-1);
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+
+            dgvData.Columns.Clear();
+            dgvData.Columns.Add("DATE", "Data notowania");
+            dgvData.Columns.Add("CLOSE", "Kurs zamkniecia");
+            dgvData.Columns.Add("VOLUME", "Wolumnen");
+
+            dgvData.Columns[0].Width = 250;
+            dgvData.Columns[1].Width = 180;
+            dgvData.Columns["VOLUME"].Width = 150;
+
+            string ticker = tbTicker.Text.Trim().ToLower();
+            string d1 = dtStart.Value.ToString("yyyyMMdd");
+            string d2 = dtStop.Value.ToString("yyyyMMdd");            
+            string url = $"https://stooq.pl/q/d/l/?s={ticker}&d1={d1}&d2={d2}&i=d";
+
+            try
+            {
+                WebClient webClient = new WebClient();
+                string s = webClient.DownloadString(url);
+                string[] lines = s.Split('\n');
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] items = lines[i].Split(',');
+                    if (items.Length == 6)
+                    {
+                        //Ładujemy do gridview
+                        dgvData.Rows.Add(new string[] {
+                            items[0], items[4], items[5] });
+
+
+                    }
+                }
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show($"Coś poszło nie tak: {exc.Message}");
+            }
+        }
+    }
+}
